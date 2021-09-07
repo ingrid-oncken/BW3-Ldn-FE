68acc265aa934dfd7cbe09665e453e5053306520
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-09-07 11:15:34 +0100

    boilerplate and components

---
+++ linkedin-fe/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import LnNavbar from "./components/LnNavbar"
+
+function App() {
+  return (
+    <div className="App">
+      <LnNavbar />
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ linkedin-fe/src/components/LnNavbar.jsx
@@ -0,0 +1,19 @@
+import { Navbar } from "react-bootstrap"
+
+const LnNavbar = () => (
+  <Navbar bg="light">
+    <Navbar.Brand href="#home">
+      <img
+        alt=""
+        src="/logo.svg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />
+      React Bootstrap
+    </Navbar.Brand>
+  </Navbar>
+)
+export default LnNavbar
+
+//xmlns="http://www.w3.org/2000/svg"
